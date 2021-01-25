3ce18cc95c7f8863662f4adac4fc71b6a9901887
Arunak2510 <77678372+Arunak2510@users.noreply.github.com>
2021-01-25 19:34:00 +0530

    Add files via upload

---
+++ exe3.rb
@@ -0,0 +1,23 @@
+
+puts "i will now count my chickens"
+#this below command will count hens
+puts "hens #{25+10/6}"
+#this below command will count roosters
+puts "roosters #{100 - 61 * 3 % 4}"
+puts "Now i will count my eggs"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#this below command will calculaate the eggs
+puts "is it true that 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6"
+
+puts "it is  true that 3 + 2 < 5 - 7"
+
+puts 3 + 2 < 5 - 7
+
+puts "how about some more"
+#print sentence within string  compare whether 5 is greater than -2
+puts "it is greater? #{5 > -2}"
+#print sentence within string compare  whether 5 is greater than equal to -2
+
+puts "it is greater or equal #{5 >= -2}"
+#print sentence within sting compare whether 5 is less than or equal to -2
+puts "its is less or equal? #{5 <= -2}"
